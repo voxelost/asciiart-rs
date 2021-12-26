b78e4ace5f08334eace61d734e682a8c617a0786
antoni boryna <borynaantoni@gmail.com>
2021-12-27 00:45:42 +0100

    refactor code to use any image extension

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,43 @@
-extern crate bmp;
+extern crate image;
 
-use bmp::Image;
+use image::io::Reader as ImageReader;
+use image::DynamicImage;
+use image::GenericImageView;
 use std::env;
+use std::io;
+use std::io::ErrorKind;
 
 const GRAYSCALE_CHARS: &str =
     "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^'. ";
 
 const IMG_SCALE: u32 = 1;
 
-fn main() {
+fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        panic!("No filename given!");
+        return Err(io::Error::new(ErrorKind::InvalidInput, "no filename given"));
     }
 
-    let img = bmp::open(args[1].clone()).unwrap_or_else(|e| {
-        panic!("Failed to open: {}", e);
-    });
-
+    let img = ImageReader::open(args[1].clone())?.decode().unwrap();
     let output_str = str_from_image(img);
 
     println!("{}", output_str);
+    Ok(())
 }
 
-fn str_from_image(img: Image) -> String {
-    let (size_x, size_y) = img.coordinates().last().unwrap();
+fn str_from_image(img: DynamicImage) -> String {
+    let width = img.width();
+    let height = img.height();
 
     let step_x: u32 = 4 * IMG_SCALE;
     let step_y: u32 = 9 * IMG_SCALE;
 
     let mut output_str: String = String::new();
 
-    for y in (0..size_y).step_by(step_y as usize) {
-        for x in (0..size_x).step_by(step_x as usize) {
+    for y in (0..height).step_by(step_y as usize) {
+        for x in (0..width).step_by(step_x as usize) {
             let gs = get_average_brightness(&img, x, y, step_x, step_y);
-
-            let idx = gs as i32 * GRAYSCALE_CHARS.len() as i32 / (std::u8::MAX as i32 + 1);
-
-            output_str.push(GRAYSCALE_CHARS.chars().nth(idx as usize).unwrap());
+            output_str.push(get_grayscale_char_for_u8_brightness(gs));
         }
 
         output_str.push('\n');
@@ -46,20 +46,35 @@ fn str_from_image(img: Image) -> String {
     output_str
 }
 
-fn get_average_brightness(img: &Image, coord_x: u32, coord_y: u32, step_x: u32, step_y: u32) -> u8 {
+fn get_grayscale_char_for_u8_brightness(brightness: u8) -> char {
+    let idx = brightness as i32 * GRAYSCALE_CHARS.len() as i32 / (std::u8::MAX as i32 + 1);
+    GRAYSCALE_CHARS.chars().nth(idx as usize).unwrap()
+}
+
+fn get_average_brightness(
+    img: &DynamicImage,
+    coord_x: u32,
+    coord_y: u32,
+    offset_x: u32,
+    offset_y: u32,
+) -> u8 {
     let mut accumulator: u64 = 0;
     let mut counter: u64 = 0;
-    let (size_x, size_y) = img.coordinates().last().unwrap();
 
-    for y in coord_y..(coord_y + step_y) {
-        for x in coord_x..(coord_x + step_x) {
-            if x > size_x || y > size_y {
+    let width = img.width();
+    let height = img.height();
+
+    let grayscale_img = img.grayscale();
+
+    for y in coord_y..(coord_y + offset_y) {
+        for x in coord_x..(coord_x + offset_x) {
+            if x >= width || y >= height {
                 continue;
             }
 
-            let px = img.get_pixel(x, y);
+            let px = grayscale_img.get_pixel(x, y);
 
-            accumulator += rgb_to_grayscale(px.r, px.g, px.b) as u64;
+            accumulator += px.0[0] as u64;
             counter += 1;
         }
     }
@@ -69,8 +84,4 @@ fn get_average_brightness(img: &Image, coord_x: u32, coord_y: u32, step_x: u32, 
     }
 
     (accumulator / counter) as u8
-}
-
-fn rgb_to_grayscale(r: u8, g: u8, b: u8) -> u8 {
-    (0.299 * r as f64 + 0.587 * g as f64 + 0.114 * b as f64) as u8
 }
